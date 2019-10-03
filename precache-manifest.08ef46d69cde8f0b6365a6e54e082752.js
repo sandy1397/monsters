b5f5c04e94e5b2ebaa1bdbecfbbdbe449712019b
Sandeep Yadav <sandeepyadav.ry1397@gmai.com>
2019-10-03 18:19:47 +0530

    Updates

---
+++ precache-manifest.08ef46d69cde8f0b6365a6e54e082752.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "534dee0919a642c333b1e5f40274aa9b",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "20a0da06da0a0096bf6c",
+    "url": "/monsters/static/css/main.4da98b6b.chunk.css"
+  },
+  {
+    "revision": "cea528bd8a6c6ccbefab",
+    "url": "/monsters/static/js/2.9918f956.chunk.js"
+  },
+  {
+    "revision": "20a0da06da0a0096bf6c",
+    "url": "/monsters/static/js/main.b6492728.chunk.js"
+  },
+  {
+    "revision": "0b202f8c0ed788dad152",
+    "url": "/monsters/static/js/runtime~main.39cb6628.js"
+  }
+]);
